0199875a24aaaf92c33bcdd7458dbe38814a0c44
Laura Adam <laurawynosky@gmail.com>
2018-05-28 19:36:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,18 @@
 def speak_to_grandma 
   if phrase=="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase==phrase.downcase
+  elsif phrase==phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase==phrase.downcase
+  elsif phrase==phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-    
+  elsif phrase==phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
+
+speak_to_grandma ("HELLO THERE")
     
     
     phrase.upcase
