54f553c8d4ebc9cabd0f2c322532f06e2b24c285
Laura Adam <laurawynosky@gmail.com>
2018-05-28 19:18:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,54 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma (phrase=="Hi Nana, how are you?")
+def speak_to_grandma 
+  if phrase =="hi nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+  if phrase == loud = phrase.upcase
+  soft = phrase.downcase
+  confuse = phrase.reverse
+  hug = "I LOVE YOU TOO PUMPKIN!"
+    
+if phrase == soft
+  puts "HUH?! SPEAK UP, SONNY!."
+elsif phrase == loud
+  puts "NO, NOT SINCE 1938!."
+elsif hug == "I LOVE YOU GRANDMA"
+  puts hug
+  puts "I LOVE YOU TOO PUMPKIN!"
+else
+  puts "WHATS FOR LUNCH"
+  
+  
+  
+  
+  phrase=="Hi Nana, how are you?"
 end
+
+greeting = "friendly_greeting"
+ 
+case greeting
+  when "unfriendly_greeting"
+    puts "What do you want!?"
+  when "friendly_greeting"
+    puts "Hi! How are you?"
+end
+
+
+if name == "Alice"
+  puts "Hello, Alice!"
+elsif name == "The White Rabbit"
+  puts "Don't be late, White Rabbit"
+elsif name == "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+elsif name == "The Queen of Hearts"
+  puts "Please don't chop off my head!"
+else
+  puts "Whoooo are you?"
+end 
+
+
+
   loud=phrase.upcase
   soft=phrase.downcase
   confuse=phrase.reverse
